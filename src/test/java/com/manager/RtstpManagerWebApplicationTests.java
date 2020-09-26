764366a426fcece61d30a6ee3fe212a9afa30788
yangkai <yangkai@easthome.com>
2020-09-26 18:55:48 +0800

    bug

--- src/test/java/com/manager/RtstpManagerWebApplicationTests.java
+++ src/test/java/com/manager/RtstpManagerWebApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ppm;
+package com.manager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RtstpManagerWebApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
