bfaa54f58f82f3fd2fdd266006396a96b2dbb114
yangkai <yangkai@easthome.com>
2020-09-29 13:48:35 +0800

    bug

---
+++ src/main/java/com/ppm/controller/MiniCodeController.java
@@ -0,0 +1,51 @@
+package com.ppm.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.ppm.constants.Constant;
+import com.ppm.entity.*;
+import com.ppm.exception.code.BaseResponseCode;
+import com.ppm.mapper.*;
+import com.ppm.service.RedisService;
+import com.ppm.utils.*;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * function 小程序登陆
+ * Created by gff on 2017/8/21.
+ */
+@Controller
+@RequestMapping(value="/api/code")
+public class MiniCodeController {
+    private final Log log = LogFactory.getLog(getClass());
+
+    final private String appid = "wxa3234f15461aface";
+    final private String secret = "243d9b2852cda6e3a812846a7cae249f";
+
+    @RequestMapping(value = "getCode", method = RequestMethod.GET)
+    public void getCode(HttpServletRequest request,String activityId, HttpServletResponse response) throws IOException {
+        String accessToken = WeiXinUtil.getAccessToken(appid,secret);
+        String codeUrl = "https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token="+accessToken;
+        Map<String,Object> param = new HashMap<>();
+        param.put("scene",activityId);
+        param.put("page","pages/logon/logon");
+        HttpTools.doImgPost(codeUrl, param);
+    }
+
+
+
+}
---
+++ src/main/java/com/ppm/utils/WeiXinUtil.java
@@ -0,0 +1,112 @@
+package com.ppm.utils;
+
+import com.alibaba.fastjson.JSONObject;
+import sun.misc.BASE64Decoder;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.spec.AlgorithmParameterSpec;
+
+public class WeiXinUtil {
+
+    // 获取token
+    public static String getAccessToken(String appid,String serect) {
+        String access_token = "";
+        String grant_type = "client_credential";// 获取access_token填写client_credential
+        // 这个url链接地址和参数皆不能变
+        String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=" + grant_type + "&appid=" + appid + "&secret="
+                + serect; // 访问链接
+
+        try {
+            URL urlGet = new URL(url);
+            HttpURLConnection http = (HttpURLConnection) urlGet.openConnection();
+            http.setRequestMethod("GET"); // 必须是get方式请求
+            http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            http.setDoOutput(true);
+            http.setDoInput(true);
+            /*
+             * System.setProperty("sun.net.client.defaultConnectTimeout",
+             * "30000");// 连接超时30秒
+             * System.setProperty("sun.net.client.defaultReadTimeout", "30000");
+             * // 读取超时30秒
+             */
+            http.connect();
+            InputStream is = http.getInputStream();
+            int size = is.available();
+            byte[] jsonBytes = new byte[size];
+            is.read(jsonBytes);
+            String message = new String(jsonBytes, "UTF-8");
+            JSONObject demoJson = JSONObject.parseObject(message);
+            access_token = demoJson.getString("access_token");
+            is.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return access_token;
+    }
+
+    // 获取ticket
+    public static String getTicket(String access_token) {
+        String ticket = null;
+        String url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=" + access_token + "&type=jsapi";// 这个url链接和参数不能变
+        try {
+            URL urlGet = new URL(url);
+            HttpURLConnection http = (HttpURLConnection) urlGet.openConnection();
+            http.setRequestMethod("GET"); // 必须是get方式请求
+            http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            http.setDoOutput(true);
+            http.setDoInput(true);
+            System.setProperty("sun.net.client.defaultConnectTimeout", "30000");// 连接超时30秒
+            System.setProperty("sun.net.client.defaultReadTimeout", "30000"); // 读取超时30秒
+            http.connect();
+            InputStream is = http.getInputStream();
+            int size = is.available();
+            byte[] jsonBytes = new byte[size];
+            is.read(jsonBytes);
+            String message = new String(jsonBytes, "UTF-8");
+            JSONObject demoJson = JSONObject.parseObject(message);
+            ticket = demoJson.getString("ticket");
+            is.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ticket;
+    }
+
+
+    /*public static String decrypt(String encrypdata, String ivdata, String sessionkey) throws Exception {
+        byte[] encData = Base64.decode(encrypdata);
+        byte[] iv = Base64.decode(ivdata);
+        byte[] key = Base64.decode(sessionkey);
+
+        AlgorithmParameterSpec ivSpec = new IvParameterSpec(iv);
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+        //解析解密后的字符串  
+        JSONObject jsonObject = JSONObject.parseObject(new String(cipher.doFinal(encData),"UTF-8"));
+        return jsonObject.get("phoneNumber").toString();
+    }*/
+
+    public static String decryptS5(String sSrc, String encodingFormat, String sKey, String ivParameter) throws Exception {
+        try {
+            BASE64Decoder decoder = new BASE64Decoder();
+            byte[] raw = decoder.decodeBuffer(sKey);
+            SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+            IvParameterSpec iv = new IvParameterSpec(decoder.decodeBuffer(ivParameter));
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+            byte[] myendicod = decoder.decodeBuffer(sSrc);
+            byte[] original = cipher.doFinal(myendicod);
+            String originalString = new String(original, encodingFormat);
+            return originalString;
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+
+}
